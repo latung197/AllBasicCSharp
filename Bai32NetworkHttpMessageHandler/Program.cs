0b8b70de59443121d74f7b17797eee6df9d343da
DESKTOP-9F5APHK\Admin <levantung0000@gmail.com>
2021-09-05 23:05:38 +0700

    Entyti

---
+++ Bai32NetworkHttpMessageHandler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Bai32NetworkHttpMessageHandler
+{
+    public class Program
+    {
+        /*
+         * Lớp HttpMessageHandler là lớp trừu tượng, nó là lớp cơ sở được thư viện .NET Core triển khai ra các lớp như DelegatingHandler, HttpMessageHandler, HttpClientHandler ... các lớp triển khai này (hoặc nếu tự xây dựng lớp triển khai HttpMessageHandler) thì phải nạp chồng phương thức SendAsync:
+         */
+         public static async Task<string> GetData()
+        {
+
+            var url = "https://postman-echo.com/post";
+
+            var httpCliet = new HttpClient();
+            var httpRequesMessage = new HttpRequestMessage();
+            httpRequesMessage.Method = HttpMethod.Post;
+            httpRequesMessage.RequestUri = new Uri(url);
+            httpRequesMessage.Headers.Add("User-Agent", "Mozilla/5.5");
+            var paremeter = new List<KeyValuePair<string, string>>();
+            paremeter.Add(new KeyValuePair<string, string>("key1", "value1"));
+            paremeter.Add(new KeyValuePair<string, string>("key2", "value2"));
+            paremeter.Add(new KeyValuePair<string, string>("key3", "value3"));
+            httpRequesMessage.Content = new FormUrlEncodedContent(paremeter);
+            var repon = await httpCliet.SendAsync(httpRequesMessage);
+            var html = await repon.Content.ReadAsStringAsync();
+            Console.WriteLine(html);
+            return html;
+        }
+
+        //public static async Task<string> GetCookies()
+        //{
+        //    var url = "https://postman-echo.com/post";
+        //    var cookies = new CookieContainer();
+
+        //}
+        static void Main(string[] args)
+        {
+           Console.WriteLine(GetData().Result);
+            
+            Console.WriteLine("hii");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bai33NetworkHttplistener/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai33NetworkHttplistener
+{
+    class Program
+    {
+        public static async Task<string> CreateServer()
+        {
+            var server = new HttpListener();
+            server.Prefixes.Add("http://*:8080/");
+            server.Start();
+            Console.WriteLine("Openserver");
+            do
+            {
+                var context = await server.GetContextAsync();
+                var reponen = context.Response;
+                var outputstream = reponen.OutputStream;
+                var html = "<H1>Xin Chao </H1>";
+                var bytes = Encoding.UTF8.GetBytes(html);
+                await outputstream.WriteAsync(bytes, 0, bytes.Length);
+                outputstream.Close();
+            }
+            while (server.IsListening);
+            return "aaa";
+        }
+        static void Main(string[] args)
+        {
+            if(HttpListener.IsSupported)// Kiểm tra xem hệ thống có hõ trợ HttpListener không
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            var a =  CreateServer();
+            a.Wait();
+            Console.ReadKey();
+        }
+    }
+}
